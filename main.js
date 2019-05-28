cd7458ed01f2a9da1c82d48d0523164077d5f2bb
Stas Tselovanskyi <solmirx1@gmail.com>
2019-05-28 12:01:54 +0300

    fixedSomeErrors

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 'use strict';
 
-const eighth = () => {
-	document.onclick = (event) => {
-	const removedElement = document.elementFromPoint(event.clientX, event.clientY);
-	removedElement.remove();}
-}
+document.addEventListener('click', (event) => {  /*I know right now we can do without argument "event" 
+  but this way my code more readable, as I think*/  
+  const removedElement = document.elementFromPoint(event.clientX, event.clientY);
+  removedElement.remove();
+  });
 
-eighth();
